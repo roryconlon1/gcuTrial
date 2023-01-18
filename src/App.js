2784c2ef34bea86d5ea0a702868af00486fee528
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2023-01-18 17:36:00 +0000

    data from api, next add to favourite and look better

--- src/App.js
+++ src/App.js
@@ -5,25 +5,33 @@ import { useCallback, useEffect, useState } from 'react';
 function App() {
 
   const [words, setWords] = useState([]);
+  const [searchWord, setSearchWord] = useState([]);
 
-  useEffect(() => {
-    getWords()
-  }, [])
+  // useEffect(() => {
+  //   getWords()
+  // }, [])
 
   const getWords = () => {
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + searchWord)
     .then(res => res.json())
     .then(data => setWords(data))
   }
 
   const helloWord = words.map((hello) => {
-    return <div>{hello.word}</div>
+    return <div><p>{hello.word}</p>
+    <p>{hello.meanings[0].definitions[0].definition}</p>
+    </div>
   })
+
+  const handleChange = (event) => {
+      setSearchWord(event.target.value)
+  }
 
 
   return (
     <div className="App">
-      <input type="text"/>
+      <input type="text" onChange={handleChange}/>
+      <button onClick={getWords}>Search</button>
       {helloWord}
     </div>
   );
